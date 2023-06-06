150f70168ebb6454024c25596c53c14e2af5b899
Oguzhan Aydin <moritanyali26@yahoo.com>
2023-06-06 20:31:52 +0300

    02/06/23

--- src/test/java/techproed/day07_MavenJUnit/C01_ClassWork.java
+++ src/test/java/techproed/day07_MavenJUnit/C01_ClassWork.java
@@ -1,12 +1,13 @@
-package techproed.day06_Maven;
+package techproed.day07_MavenJUnit;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class C02_ClassWork {
+public class C01_ClassWork {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -18,15 +19,26 @@ public class C02_ClassWork {
 //        1.http://zero.webappsecurity.com sayfasina gidin
           driver.get("http://zero.webappsecurity.com");
 //        2.Signin buttonuna tiklayin
+          driver.findElement(By.xpath("//button[@id='signin_button']")).click();
 //        3.Login alanine “username” yazdirin
-//        4.Password alanine “password” yazdirin
-//        5.Sign in buttonuna tiklayin
-//        6.Online Banking altinda Pay Bills sayfasina gidin
-//        7.amount kismina yatirmak istediginiz herhangi bir miktari yazin
-//        8.tarih kismina “2020-09-10” yazdirin
-//        9.Pay buttonuna tiklayin
-//        10.“The payment was successfully submitted.” mesajinin ciktigini control edin
+          driver.findElement(By.xpath("//*[@tabindex='1']")).sendKeys("username");
 
+//        4.Password alanine “password” yazdirin
+        driver.findElement(By.xpath("//*[@tabindex='2']")).sendKeys("password");
+//        5.Sign in buttonuna tiklayin
+        driver.findElement(By.xpath("//*[@value='Sign in']")).click();
+        driver.navigate().back();
+//        6.Online Banking altinda Pay Bills sayfasina gidin
+        driver.findElement(By.xpath("(//strong)[2]")).click();
+//        7.amount kismina yatirmak istediginiz herhangi bir miktari yazin
+        driver.findElement(By.xpath("(//span[@id='pay_bills_link'])[1]")).click();
+        driver.findElement(By.xpath("//*[@id='sp_amount']")).sendKeys("100");
+//        8.tarih kismina “2020-09-10” yazdirin
+        driver.findElement(By.xpath("//*[@class='span4 hasDatepicker']")).sendKeys("2020-09-10");
+//        9.Pay buttonuna tiklayin
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+//        10.“The payment was successfully submitted.” mesajinin ciktigini control edin
+        System.out.println("Is payment succesfull? " +driver.findElement(By.xpath("//*[text()='The payment was successfully submitted.']")).isDisplayed());
 
     }
 }
