696d5423f1340eeb8d892b8ccbd6f77577b6d9bf
Oguzhan Aydin <moritanyali26@yahoo.com>
2023-06-25 01:05:46 +0300

    24/06/23

--- src/test/java/techproed/utilities/TestBase.java
+++ src/test/java/techproed/utilities/TestBase.java
@@ -1,5 +1,8 @@
 package techproed.utilities;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
@@ -11,6 +14,9 @@ import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -20,12 +26,16 @@ import java.util.Date;
 import java.util.List;
 
 public abstract class TestBase {
+    protected ExtentReports extentReports; //-->Raporlamayı başlatmak için kullanılan class
+    protected ExtentHtmlReporter extentHtmlReporter;//-->Raporu HTML formatında düzenler
+    protected ExtentTest extentTest;//--> Test adınlarına eklemek istediğimiz bilgileri bu class ile oluştururuz
     /*
-    TestBase class'ından obje oluşturmanın önüne geçmek için bu class'ı abstract yapabiliriz.
-    TestBase testBase new = TestBase(); yani bu sekilde obje olusturmanin onune gecmis oluruz
-    Bu class'a extends yaptigimiz test class'larindan ulabiliriz
+        TestBase class'ından obje oluşturmanın önüne geçmek için bu class'ı abstract yapabiliriz.
+    TestBase testBase = new TestBase(); yani bu şekilde obje oluşturmanın önüne geçmiş oluruz.
+    Bu class'a extends yaptığımız test class'larından ulaşabiliriz
      */
     protected WebDriver driver;
+
     @Before
     public void setUp() throws Exception {
         WebDriverManager.chromedriver().setup();
@@ -36,7 +46,8 @@ public abstract class TestBase {
 
     @After
     public void tearDown() throws Exception {
-                driver.quit();
+        //driver.quit();
+        //extentReports.flush();
     }
 
     //HARD WAIT (Bekleme Methodu)
@@ -49,96 +60,144 @@ public abstract class TestBase {
     }
 
     //Selenium Wait/Explicit Wait
-    //visibility(element) methodu
-    public void visibleWait(WebElement element, int saniye){
+    //visibilityOf(element) methodu
+    public void visibleWait(WebElement element, int saniye) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(saniye));
         wait.until(ExpectedConditions.visibilityOf(element));
     }
 
     //visibilityOfElementLocated(locator) methodu
-    public void visibleWait(By locator, int saniye){
+    public void visibleWait(By locator, int saniye) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(saniye));
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
     //AlertWait methodu
-    public void alertWait(int saniye){
+    public void alertWait(int saniye) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(saniye));
         wait.until(ExpectedConditions.alertIsPresent());
     }
 
     //FluentWait visible Methodu
-    public void visibleFluentWait(WebElement element,int saniye,int milisaniye){
+    public void visibleFluentWait(WebElement element, int saniye, int milisaniye) {
         new FluentWait<>(driver).withTimeout(Duration.ofSeconds(saniye)).
                 pollingEvery(Duration.ofMillis(milisaniye)).
                 until(ExpectedConditions.visibilityOf(element));
     }
 
+
     //AcceptAlert
-    public void acceptAlert(){
+    public void acceptAlert() {
         driver.switchTo().alert().accept();
     }
 
     //DismissAlert
-    public void dismissAlert(){
+    public void dismissAlert() {
         driver.switchTo().alert().dismiss();
     }
-    //getTextAlert
-    public String getTextAlert(){
-       return driver.switchTo().alert().getText();
 
+    //getTextAlert
+    public String getTextAlert() {
+        return driver.switchTo().alert().getText();
     }
+
     //sendKeysAlert
-    public void sendKeysAlert(String text){
+    public void sendKeysAlert(String text) {
         driver.switchTo().alert().sendKeys(text);
     }
+
     //DropDown VisibleText
-    public void selectVisibleText(WebElement ddm, String text){
+    public void selectVisibleText(WebElement ddm, String text) {
         Select select = new Select(ddm);
         select.selectByVisibleText(text);
     }
+
     //DropDown Index
-    public void selectIndex(WebElement ddm, int index){
+    public void selectIndex(WebElement ddm, int index) {
         Select select = new Select(ddm);
         select.selectByIndex(index);
     }
+
     //DropDown Value
-    public void selectValue(WebElement ddm, String value){
+    public void selectValue(WebElement ddm, String value) {
         Select select = new Select(ddm);
         select.selectByValue(value);
     }
 
-    //SwtichTo Window-1
-    public void switchToWindow(int index){
+    //SwitchTo Window-1
+    public void switchToWindow(int index) {
+        List<String> pencereler = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(pencereler.get(index));
+    }
+
+    //SwitchTo Window-2
+    public void switchWindow(int index) {
         driver.switchTo().window(driver.getWindowHandles().toArray()[index].toString());
     }
 
-    //SwtichTo Window-2
-    public void switchToWindow2(int index){
-       List<String> pencereler = new ArrayList<>(driver.getWindowHandles());
-       driver.switchTo().window(pencereler.get(index));
-    }
-
     //Tüm Sayfa Resmi (ScreenShot)
-    public void tumSayfaResmi(){
+    public void tumSayfaResmi() {
         String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
         String dosyaYolu = "src/test/java/techproed/TumSayfaResmi/screenShot" + tarih + ".jpeg";
         TakesScreenshot ts = (TakesScreenshot) driver;
         try {
-            FileUtils.copyFile(ts.getScreenshotAs(OutputType.FILE),new File(dosyaYolu));
+            FileUtils.copyFile(ts.getScreenshotAs(OutputType.FILE), new File(dosyaYolu));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    //WebElement Resmi (Webelement ScreenShot)
+    public void webElementResmi(WebElement element) {
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "src/test/java/techproed/ElementResmi/WEscreenShot" + tarih + ".jpeg";
+        try {
+            FileUtils.copyFile(element.getScreenshotAs(OutputType.FILE), new File(dosyaYolu));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    //WebElement Resmi (Webelement ScreenShot)
-    public void webElementResmi(WebElement element){
-        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
-        String dosyaYolu = "src/test/java/techproed/ElementResmi/WEscreenShot" + tarih + ".jpeg";
+    //UploadFile Robot Class
+    public void uploadFilePath(String filePath) {
         try {
-            FileUtils.copyFile(element.getScreenshotAs(OutputType.FILE),new File(dosyaYolu));
-        } catch (IOException e) {
+            bekle(3);
+            StringSelection stringSelection = new StringSelection(filePath);
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+            Robot robot = new Robot();
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            bekle(3);
+            robot.keyPress(KeyEvent.VK_V);
+            bekle(3);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            bekle(3);
+            robot.keyRelease(KeyEvent.VK_V);
+            bekle(3);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            bekle(3);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+            bekle(3);
+        } catch (AWTException e) {
             throw new RuntimeException(e);
         }
     }
+
+    //Extent Report Methodu
+    public void extentReport(String browser, String reportName) {
+        extentReports = new ExtentReports();
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "testOutput/extentReports/extentReport" + tarih + ".html";
+        extentHtmlReporter = new ExtentHtmlReporter(dosyaYolu);
+        extentReports.attachReporter(extentHtmlReporter);//-->HTML formatında raporlamayı başlatacak
+
+        //Raporda gözükmesini isteğimiz bilgiler için
+        extentReports.setSystemInfo("Browser", browser);
+        extentReports.setSystemInfo("Tester", "Erol");
+        extentHtmlReporter.config().setDocumentTitle("Extent Report");
+        extentHtmlReporter.config().setReportName(reportName);
+
+
+    }
+
 }
