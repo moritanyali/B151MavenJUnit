a6c38ec6c98547e3f9f5e874269a286555bd146d
Oguzhan Aydin <moritanyali26@yahoo.com>
2023-07-07 21:53:38 +0300

    07/07/23

---
+++ src/test/java/techproed/day23_Log4j/C01_log4j.java
@@ -0,0 +1,31 @@
+package techproed.day23_Log4j;
+
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class C01_log4j {
+
+    @Test
+    public void log4j() {
+         /*
+        Log4j system.out.println gibi her test adiminda bilgilendirme yapmamiza yarayan bir api' dir
+            Log4j ile bu bilgilendirmeleri hem konsolda tarih saat olarak hem de src>main>resources icine
+        koydugumuz log4j.xml file da belirttigimiz .log uzantili dosyada yine tarih saat olarak kayit altina
+        almis olacagiz.
+            Log4j kullanabilmek icin oncelikle log4j-api ve log4j-core dependency lerini mvnrepository.com
+        adresinden (ayni version numaralarina sahip ) alip pom.xml dosyamiza koymaliyiz.
+            src>main>resources icine koydugumuz log4j.xml dosyasindaki kodlara pdf deki adresten ulasabilirsiniz
+
+         */
+
+        Logger logger = LogManager.getLogger(C01_log4j.class);
+        logger.info("INFO");
+        logger.debug("DEBUG");
+        logger.error("ERROR");
+        logger.fatal("FATAL");
+        logger.warn("WARNING");
+
+    }
+}
---
+++ src/test/java/techproed/day23_Log4j/C02_Log4j.java
@@ -0,0 +1,25 @@
+package techproed.day23_Log4j;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import techproed.utilities.TestBase;
+
+public class C02_Log4j extends TestBase {
+    @Test
+    public void test01() {
+        Logger logger = LogManager.getLogger(C02_Log4j.class);
+        //Techproeducation sayfasina gidelim
+        driver.get("https://techproeducation.com");
+        logger.info("Techpro sayfasına gidildi.");
+        driver.findElement(By.xpath("//i[@class='eicon-close']")).click();
+        logger.info("Sayfada çıkan reklam kapatıldı.");
+
+        //Basligin egitim icerdigini dogrulayalim
+        Assert.assertFalse(driver.getTitle().contains("Egitim"));
+        logger.error("Sayfa başlığı Egitim yazisi içermiyor");
+
+    }
+}
